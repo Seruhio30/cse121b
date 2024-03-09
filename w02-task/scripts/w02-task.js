3ccfa1d67d6f32876ccb3b0b2822658f7084edeb
Sergio Herrera Rivera <sh0041314@gmail.com>
2024-03-09 13:05:58 -0600

    w02 Task ready

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,29 +1,36 @@
-/* W02-Task - Profile Home Page */
+document.addEventListener('DOMContentLoaded', function() {
+    let fullName = 'Sergio Herrera Rivera';
+    let currentYear = '2024';
+    let profilePicture = 'images/sergio.jpeg';
 
-/* Step 1 - Setup type tasks - no code required */
+    const nameElement = document.getElementById('name');
+    const foodElement = document.getElementById('food');
+    const yearElement = document.querySelector('#year');
+    const imageElement = document.getElementById("profilePicture");
 
-/* Step 2 - Variables */
+    nameElement.innerHTML = `<strong>${fullName}</strong>`;
+    yearElement.textContent = currentYear;
+    
+    if(imageElement) { 
+        imageElement.setAttribute('src', profilePicture); 
+        imageElement.setAttribute('alt', 'Profile image of ${fullName}');
+    } else {
+        console.error("No se encontró el elemento con ID 'profilePicture'.");
+    }
+    
+    var myFavoriteFood = ['Pasta', 'Ice-cream', 'Japanese food', 'Pizza', 'Gallo Pinto', 'Tamales'];
+
+    const anotherFavorityFood = "sushi";
+    myFavoriteFood.push(anotherFavorityFood);
+
+    myFavoriteFood.shift();
+
+    myFavoriteFood.pop();
+    foodElement.innerHTML += `<br>${myFavoriteFood.join("<br>")}`;
+
+    
 
 
+    
 
-
-/* Step 3 - Element Variables */
-
-
-
-
-
-/* Step 4 - Adding Content */
-
-
-
-
-
-
-/* Step 5 - Array */
-
-
-
-
-
-
+})
